61b2435dae4a3296474f3b3642b37f5467cd80f2
LauraOchoa <lauraochoae7@gmail.com>
2020-05-28 00:13:22 -0500

    Primera version

---
+++ index.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+#Librerias usadas
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd 
+import plotly.express as px
+from sodapy import Socrata
+
+from app import app, server
+
+
+client = Socrata("www.datos.gov.co", None)   
+results = client.get("gt2j-8ykr", limit=1000000)  #Limit of 1000000 rows
+
+#Lectura de los datos
+df = pd.DataFrame.from_records(results)
+###Cambio de las fechas a tipo datetime
+df['fecha_diagnostico']=pd.to_datetime(df['fecha_diagnostico'],errors='coerce')
+####Agrupación de casos
+Cases=df.groupby('fecha_diagnostico')['id_de_caso'].count().to_frame().reset_index().rename({'id_de_caso':'Casos'},axis='columns')
+Cases['Acum']=Cases['Casos'].cumsum()
+col_options=[dict(label=x,value=x) for x in Cases.columns[1:3]]
+
+####Agrupación de estado de los contagiados
+df['estado']=df['estado'].replace({'leve':'Leve'})
+df_estado=df.groupby('estado')['id_de_caso'].count().to_frame().rename({'id_de_caso':'Cant'},axis='columns')
+####Agrupacioón de la atención dada a los contagiados
+df_atencion=df.groupby(['atenci_n'])['id_de_caso'].count().to_frame().reset_index().rename({'id_de_caso':'Casos'}, axis='columns')
+
+####Creacion layout
+app = dash.Dash(__name__)
+
+app.layout=html.Div(children=[html.H1('Covid-19 Colombia',style= {'text-align':'center'}),
+                             
+    
+                    dcc.Dropdown(id='y',options=col_options,multi=False,value='Casos'),
+                    html.Div(id='informacion'),
+                    html.Br(),
+                    dcc.Graph(id='graph',figure={}),
+                    html.H2('Estado de los contagiados',style= {'text-align':'center'}),
+                    dcc.Graph(id='graph2',figure=px.pie(df_estado,values='Cant',names=df_estado.index)),
+                    html.H2( 'Atencion de los contagiados',style= {'text-align':'center'}),      
+                    dcc.Graph(id='graph3',figure=px.pie(df_atencion,values='Casos',names='atenci_n',hole=0.3)),
+                    
+                    
+                    ]
+                    )
+
+####Llamada a la función de selección 
+@app.callback([Output(component_id='informacion',component_property='children'),
+               Output(component_id='graph', component_property='figure')], 
+              [Input(component_id='y', component_property='value')])
+def update_graph(option_slctd):
+    
+    print(option_slctd)
+
+    info = "A continuación se muestran varios gráficos sobre los casos de Covid en Colombia"
+    
+    fig=px.bar(Cases, x='fecha_diagnostico', y=option_slctd)
+    
+    
+    
+    return info, fig
+
+
+
+
+if __name__== '__main__':
+    app.run_server(debug=True)
